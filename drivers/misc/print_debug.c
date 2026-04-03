f013f64264bd2a24d7a75ad1fcdb046fe5669d0f
devrim6 <72068078+devrim6@users.noreply.github.com>
2026-04-03 14:03:25 +0300

    a

--- drivers/misc/print_debug.c
+++ drivers/misc/print_debug.c
@@ -16,7 +16,7 @@ void print_debug(const char* fmt, ...){
     #if DEBUG_PRINT_ENABLED
     va_list args;
     va_start(args, fmt);
-    vprintf(fmt, args);
+    vprintf(fmt, args); // test ceedling thing
     va_end(args);
     #endif
 }
