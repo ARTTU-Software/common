c9d6a3e4e7ccb543161339db5363c4d4320cb65a
devrim6 <72068078+devrim6@users.noreply.github.com>
2026-04-03 18:17:14 +0300

    move voltage calc (#8)

--- drivers/sensors/Generic/generic_linear_sensor.c
+++ drivers/sensors/Generic/generic_linear_sensor.c
@@ -1,14 +1,15 @@
 #include "generic_linear_sensor.h"
 
 void generic_linear_update(Generic_Linear_Sensor_t *sensor, uint16_t adc_value) {
+
+    // 12bit ADC + 3.3V ref.
+    sensor->voltage = adc_value * (3.3f / 4095.0f);
+
     if (adc_value < sensor->min_adc) {
         adc_value = sensor->min_adc;
     } else if (adc_value > sensor->max_adc) {
         adc_value = sensor->max_adc;
     }
-
-    // 12bit ADC + 3.3V ref.
-    sensor->voltage = adc_value * (3.3f / 4095.0f);
 
     if (sensor->max_adc == sensor->min_adc) {
         sensor->displacement = sensor->min_real;
